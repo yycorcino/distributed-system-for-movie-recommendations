09fa87632e433468696355e937d0138520229601
Sebastian Corcino <corcinosebastian@outlook.com>
2024-10-13 00:35:01 -0800

    improve readme, add ability to take arguments
    Co-authored-by: Gaozong Lo <logaozong@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-## Distributed System For Movie Recommendations
+<a name="readme-top"></a>
 
-Developed by: Sebastian Corcino and Gaozong Lo
+<!-- PROJECT LOGO -->
+<div align="center">
+  <h3 align="center">Distributed System For Movie Recommendations</h3>
+
+  <p align="center">
+   Developed by: Sebastian Corcino and Gaozong Lo
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+This project uses Google Cloud Platform(GCP) to host Apache Spark and Hadoop via Dataproc. Utilizing the Google Console, we are able to submit jobs to emulate the user loading home there "For You Page".
+
+### Usage
+
+How to submit jobs?
+
+- Submit a job via Google Console
+
+  ```
+    gcloud dataproc jobs submit pyspark <gs://genre-movies/rec_movie.py> --cluster=<apache-hadoop-instance> --region=<us-central1> -- <user_id>
+  ```
+
+    _Replace <{data}> according to your setup._
+
+### Creating Data
+
+How to create your own data with [The Movie Database](https://developer.themoviedb.org/docs/getting-started)?
+
+1. Create API Key for TMDB
+
+   ```
+   API_KEY=1234
+   ```
+    _Put inside .env file_
+
+2. Run fetch_movies.py
+
+    ```
+    python3 fetch_movies.py
+    ```
+
+3. Run create_usr_data.py
+    ```
+    python3 create_usr_data.py
+    ```
+    _Edit variable "user_genre_preferences" for custom user movie list._
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- rec_movie.py
+++ rec_movie.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import random
+import sys
 
-#for genre ids
+# for genre ids
 genre_mapping = {
     28: "Action",
     12: "Adventure",
@@ -24,18 +24,30 @@ genre_mapping = {
     37: "Western"
 }
 
-def get_user_genres(user_rated_movies): #list of genres collected based on user list
+def get_user_genres(user_rated_movies): 
+    """
+    List of genres collected based on user list.
+    """
+
     user_genres = []
     for genre_ids in user_rated_movies['genre_id']:
         genres = eval(genre_ids)  
         user_genres.extend(genres)  
     return set(user_genres) 
 
-def genre_check(genre_ids, user_genres): #checks of genres of movies recommended align with user interest
+def genre_check(genre_ids, user_genres):
+    """
+    Checks genres of movies recommended align with user interest.
+    """
+
     movie_genres = eval(genre_ids)
     return any(int(genre) in user_genres for genre in movie_genres)
 
-def get_genres(genre_ids): #checks for genre id and its matching genre type for output
+def get_genres(genre_ids):
+    """
+    Checks for genre id and its matching genre type for output.
+    """
+
     genres = eval(genre_ids)
     return [genre_mapping[int(genre)] for genre in genres]
 
@@ -50,14 +62,12 @@ def get_recommendations(user_id):
 
     recommended_movies = movies_df[movies_df['vote_average'] >= 5]
     recommended_movies = recommended_movies[recommended_movies['genre_ids'].apply(lambda x: genre_check(x, user_genres))]
-
     recommended_movies['genres'] = recommended_movies['genre_ids'].apply(get_genres)
 
     output_columns = ['id', 'title', 'release_date', 'vote_average', 'genres']
     top_recommendations = recommended_movies[output_columns]
 
     randomized_recommendations = top_recommendations.sample(frac=1).reset_index(drop=True)  
-
     top_random_recommendations = randomized_recommendations.head(10)
 
     print(f"\nRecommended Movies for User {user_id}:\n")
@@ -69,9 +79,11 @@ def get_recommendations(user_id):
         print(f"Genres: {', '.join(row['genres'])}\n")
 
 if __name__ == "__main__":
-    #edit for hadoop 
-    movies_df = pd.read_csv('master_movies.csv')
-    user_ratings_df = pd.read_csv('users_list.csv')
+    print("Starting ...")
 
-    user_input_id = int(input("Enter the User ID for a list of 10 recommendations based on previous movie ratings: "))
+    # edit the file path for Hadoop 
+    movies_df = pd.read_csv('gs://genre-movies/master_movies.csv')
+    user_ratings_df = pd.read_csv('gs://genre-movies/users_list.csv')
+
+    user_input_id = int(sys.argv[1]) 
     get_recommendations(user_input_id)
